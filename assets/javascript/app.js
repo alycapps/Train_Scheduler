57209b024596b667c60f7d7191972600ea425d8f
Aly <38168385+alycapps@users.noreply.github.com>
2018-07-17 16:28:31 -0400

    connecting to firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
     //connect to firebase
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBonZbwymHysA8SJ6zRwsbOFZDWU26tu5g",
+    authDomain: "train-schedule-d685b.firebaseapp.com",
+    databaseURL: "https://train-schedule-d685b.firebaseio.com",
+    projectId: "train-schedule-d685b",
+    storageBucket: "train-schedule-d685b.appspot.com",
+    messagingSenderId: "747902248747"
+  };
+  firebase.initializeApp(config);
+
+
     //display firebase train schedule on page load
     //add train
         //add details to firebase
