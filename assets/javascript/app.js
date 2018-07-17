242e8b1c7762cb58218b361ab435c7f6c404c9b7
Aly <38168385+alycapps@users.noreply.github.com>
2018-07-16 20:26:53 -0400

    psuedocoding the js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-
+    //connect to firebase
+    //display firebase train schedule on page load
+    //add train
+        //add details to firebase
+        //add details to page
 });
