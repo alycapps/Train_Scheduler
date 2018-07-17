8c5b3883244bfe286b0c1c8bb70fc81b6d9a26ec
Aly <38168385+alycapps@users.noreply.github.com>
2018-07-17 18:01:27 -0400

    working on setting data in firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,20 +17,33 @@
 
     //initial train object
     var schedule = {
-        train0: "name, destination, time, frequency",
-        train1: "name, destination, time, frequency"
+        train0: ["name, destination, time, frequency"],
+        train1: ["name, destination, time, frequency"]
     };
 
-
     //add train
-    trainname = $("#trainnameloc").val().trim();
-    // push to train array based on length of schedule
-    destination = $("#destinationloc").val().trim();
-    traintime = $("#traintimeloc").val().trim();
-    frequency = $("#frequencyloc").val().trim();
-    //push array to schedule obj
+    $("#submitbttnloc").on("click", function() {
+        event.preventDefault();
+        var train0 = [""];
+        var trainname = $("#trainnameloc").val().trim();
+        // push to train array based on length of schedule
+        var destination = $("#destinationloc").val().trim();
+        var traintime = $("#traintimeloc").val().trim();
+        var frequency = $("#frequencyloc").val().trim();
+        //push array to schedule obj
+        train0.push(trainname);
+        train0.push(destination);
+        train0.push(traintime);
+        train0.push(frequency);
 
-    
+        console.log("train0: " + train0);
+        // schedule.push(train0);
+        // console.log("schedule: " + schedule);
+
+        database.ref().set({
+            key: train0
+          });
+    });
         //add details to firebase
 
         //add details to page
@@ -38,7 +51,7 @@
     for (i=0; i<schedule.length; i++) {
         var trainnum = "train" + i;
         //for loop for each location in each array in schedule object
-        for (j=0, j<4, j++) {
+        for (j=0; j<4; j++) {
             var td = "<td>"
         }
     };
