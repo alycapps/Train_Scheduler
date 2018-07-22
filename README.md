301c8675d122f52f981949b7c4b34af30c86f1ec
Aly <38168385+alycapps@users.noreply.github.com>
2018-07-21 22:34:20 -0400

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Train_Scheduler
+
+This is a fun app for fake trains. Users can put in a fictitional train and its details to see when it will arrive next as well as view the details of the trains other users have entered.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,62 +1,56 @@
-
-    //connect to firebase
- // Initialize Firebase
- var config = {
+//connect to firebase
+// Initialize Firebase
+var config = {
     apiKey: "AIzaSyByQBklp5y58Hhncmc1Qr5kFdHgOweXbB0",
     authDomain: "trainscheduler-1fd87.firebaseapp.com",
     databaseURL: "https://trainscheduler-1fd87.firebaseio.com",
     projectId: "trainscheduler-1fd87",
     storageBucket: "trainscheduler-1fd87.appspot.com",
     messagingSenderId: "263195455251"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-    //display firebase train schedule on page load
-    var database = firebase.database();
+//display firebase train schedule on page load
+var database = firebase.database();
 
-    //add train
-    $("#submitbttnloc").on("click", function() {
-        event.preventDefault();
-        var trainname = $("#trainnameloc").val().trim();
-        var destination = $("#destinationloc").val().trim();
-        var traintime = $("#traintimeloc").val().trim();
-        var frequency = $("#frequencyloc").val().trim();
-   
-                //add details to firebase
-        database.ref().push({
-            name: trainname,
-            destination: destination,
-            traintime: traintime,
-            frequency: frequency,
-          });
+//add train
+$("#submitbttnloc").on("click", function() {
+    event.preventDefault();
+    var trainname = $("#trainnameloc").val().trim();
+    var destination = $("#destinationloc").val().trim();
+    var traintime = $("#traintimeloc").val().trim();
+    var frequency = $("#frequencyloc").val().trim();
 
-        //clear form after submission
-        $("#trainnameloc").val("");
-        $("#destinationloc").val("");
-        $("#traintimeloc").val("");
-        $("#frequencyloc").val("");
+    //add details to firebase
+    database.ref().push({
+        name: trainname,
+        destination: destination,
+        traintime: traintime,
+        frequency: frequency,
     });
 
+    //clear form after submission
+    $("#trainnameloc").val("");
+    $("#destinationloc").val("");
+    $("#traintimeloc").val("");
+    $("#frequencyloc").val("");
+});
 
-        //add details to page
 
-    database.ref().on("child_added", function(snapshot) {
-        console.log(snapshot.val().traintime);
-        //first train time
-        var ftime = snapshot.val().traintime;
-        console.log("ftime: " + ftime)
-        var convftime = moment(ftime, "HH:mm").subtract(1, "years");
-        var freq = snapshot.val().frequency;
-        var currentTime = moment().format("hh:mm");
-        console.log("time: " + currentTime);
-        //difference in time between now and first train
-        var timediff = moment().diff(moment(convftime), "minutes")
-        console.log("difference: " + timediff);
-        var minaway = timediff % freq;
-        var convminaway = moment(minaway).format("hh:mm")
-        console.log(convminaway);
-        var nexttime = moment().add(minaway, "minutes").format("hh:mm")
-        console.log(nexttime);
+//add details to page
+database.ref().on("child_added", function(snapshot) {
+    //first train time
+    var ftime = snapshot.val().traintime;
+    var convftime = moment(ftime, "HH:mm").subtract(1, "years");
+    var freq = snapshot.val().frequency;
+    var currentTime = moment().format("hh:mm");
 
-        $("#table").append("<tr>" + "<td>" + snapshot.val().name + "<td>" + snapshot.val().destination + "<td>" + snapshot.val().frequency + "<td>" + nexttime + "<td>" + minaway + " min" + "</td>");
-    })
+    //difference in time between now and first train
+    var timediff = moment().diff(moment(convftime), "minutes")
+
+    //Remainder of difference in time divided by frequency
+    var minaway = timediff % freq;
+    var nexttime = moment().add(minaway, "minutes").format("hh:mm")
+
+    $("#table").append("<tr>" + "<td>" + snapshot.val().name + "<td>" + snapshot.val().destination + "<td>" + snapshot.val().frequency + "<td>" + nexttime + "<td>" + minaway + " min" + "</td>");
+})
